6011a11d2ad970583d5413788521440e5e06a291
Kirill Cherepanov <kirilosochki2@gmail.com>
2026-04-17 12:29:12 +0300

    clear some dumb things from arduino side Rename p.V to p. Voltage

--- arduino_side/arduino_side.ino
+++ arduino_side/arduino_side.ino
@@ -24,9 +24,9 @@ const byte L3G4200D = 0x69; //SDO к 3.3v - 0x69, к GND - 0x68
 
 //Раз в столько мс производятся вычисления скоростей, позиций и т.п.
 
-float ax, ay, az;
-float axCor, ayCor, azCor;
-float vx, vy, vz;
+float ax, ay, az;           // Ускорения, снятые с акселл. Если есть фильтр, то уже фильтрованные
+float axCor, ayCor, azCor; // Коррекция акселерометра, устанавливается в функции AcsCall
+float vx, vy, vz;         // Вычисленные скорости и координа
 float sx, sy, sz;
 
 // --- МАГНИТОМЕТР ---
@@ -36,8 +36,9 @@ float Heading;
 
 // --- ГИРОСКОП ---
 float gx, gy, gz;
-float angx, angy, angz;
-float angrx, angry, angrz;
+float angx, angy, angz;   // Углы поворота бпла в пространстве
+float angrx, angry, angrz;  // Те же углы но в радианах 
+
 // --- ТРМОМЕТР-БАРОМЕТР ---
 int BarThermConstS[8];
 unsigned int BarThermConstU[3];
@@ -67,7 +68,7 @@ void setup() {
   RadioSetup();
   ActuatorsSetup();
   
-   Serial.println("Константы BarTherm двухбайтные, откуда при выводе берутся лишние 2 байта неясно");
+  //Serial.println("Константы BarTherm двухбайтные, откуда при выводе берутся лишние 2 байта неясно");
 
 }
 unsigned long tmr2 = millis();
@@ -125,7 +126,7 @@ static void sendPacket()
     p.P = (float)P;
     p.T = T;
     p.BarAlt = (float)BarAlt;
-    p.V = (float)(analogRead(7) * 5.0f) / 1024.0f;
+    p.Voltage = (float)(analogRead(7) * 5.0f) / 1024.0f;
     p.d = (float)data[1];
     p.Tcycle = (float)Tcycle;
 
@@ -195,9 +196,7 @@ void AcsCall() {
   ay = -(y/26.32 - ayCor);
   az = -(z/26.32 - azCor);
 
-  ax = ax;
-  ay = ay;
-  az = az;
+  // удалил ax=ax и для других осей.
 }
 
 void MagSetup() {
@@ -297,7 +296,7 @@ void GyroSetup() {
      Wire.write(GyroCTRL[i]);
      Wire.endTransmission();
    } 
-
+  // НАчальные значения углов. Не скоростей.
   angx = 0;
   angy = 0;
   angz = 90;
--- src/datasource/telemetry_protocol.h
+++ src/datasource/telemetry_protocol.h
@@ -1,46 +1,46 @@
 // datasource/telemetry_protocol.h
 #pragma once
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 enum : uint8_t {
-    TEL_SOF1 = 0xAA,
-    TEL_SOF2 = 0x55,
-    TEL_VERSION = 1,
-    TEL_TYPE_DATA = 1
+  TEL_SOF1 = 0xAA,
+  TEL_SOF2 = 0x55,
+  TEL_VERSION = 1,
+  TEL_TYPE_DATA = 1
 };
 
 // Your current packet, packed so no padding is inserted.
 #pragma pack(push, 1)
-struct DataPacket
-{
-    float time;      // time from MCU
-    float az, ay, ax;
-    float vx, vy, vz;
-    float sx, sy, sz;
-    float mx, my, mz;
-    float H;
-    float gx, gy, gz;
-    float angx, angy, angz;
-    float P;
-    float T;
-    float BarAlt;
-    float V;
-    float d;
-    float Tcycle;
+struct DataPacket {
+  float time; // time from MCU
+  float az, ay, ax;
+  float vx, vy, vz;
+  float sx, sy, sz;
+  float mx, my, mz;
+  float H;
+  float gx, gy, gz;
+  float angx, angy, angz;
+  float P;
+  float T;
+  float BarAlt;
+  float Voltage;
+  float d;
+  float Tcycle;
 };
 #pragma pack(pop)
 
-inline uint16_t crc16_ccitt(const uint8_t* data, size_t len)
-{
-    uint16_t crc = 0xFFFF;
-    for (size_t i = 0; i < len; ++i) {
-        crc ^= (uint16_t)data[i] << 8;
-        for (int b = 0; b < 8; ++b) {
-            if (crc & 0x8000) crc = (crc << 1) ^ 0x1021;
-            else              crc <<= 1;
-        }
+inline uint16_t crc16_ccitt(const uint8_t *data, size_t len) {
+  uint16_t crc = 0xFFFF;
+  for (size_t i = 0; i < len; ++i) {
+    crc ^= (uint16_t)data[i] << 8;
+    for (int b = 0; b < 8; ++b) {
+      if (crc & 0x8000)
+        crc = (crc << 1) ^ 0x1021;
+      else
+        crc <<= 1;
     }
-    return crc;
+  }
+  return crc;
 }
